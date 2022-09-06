c2e01cc2998b760c4f10bd427c905e041df42dc2
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-07 00:02:09 +0200

    07.09.2022

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const listCategory = document.querySelectorAll('.item');
+
+
+const nameCategory = listCategory.lastElementChild;
+
+
+console.log (`Number of categories: ${listCategory.length}`);
+console.log (nameCategory);
+//console.log(`Category: ${nameCategory.textContent}`);
+
+//nameCategory.forEach(element => console.log (element));
+
+//console.log (listCategory);
+
