b2cad00ec411a3885bb3288653acf61d0e5bd885
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-07 23:32:44 +0200

    07.09.2022

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,32 @@
-const listCategory = document.querySelectorAll('.item');
-
-
-const nameCategory = listCategory.lastElementChild;
-
+const listCategory = document.querySelectorAll('li.item');
 
 console.log (`Number of categories: ${listCategory.length}`);
-console.log (nameCategory);
-//console.log(`Category: ${nameCategory.textContent}`);
 
+
+const chaildAll = listCategory.forEach(function(name, number){
+return (name, number)
+});
+console.log (chaildAll);
+//const nameCategory = listCategory.querySelectorAll('h2');
+//const numberItems = listCategory.querySelectorAll('li');
+
+
+
+//nameCategory.forEach( => {document.querySelectorAll('h2')});
+    
+
+//const nameCategory = listCategory.lastElementChild;
+
+
+
+//console.log (nameCategory);
+/*console.log(`Category: ${nameCategory[0].textContent}`);
+console.log(`Elements: ${numberItems[0].length}`);
+
+console.log(`Category: ${nameCategory[1].textContent}`);
+
+console.log(`Category: ${nameCategory[2].textContent}`);
 //nameCategory.forEach(element => console.log (element));
 
-//console.log (listCategory);
+//console.log (listCategory);*/
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,5 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+//const itens.forEach (ingredients[] => {document.createElement('li')});
+console.log (itens);
