8ac7919438e8c593db23b0ba766e85d092252384
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-09 00:07:05 +0200

    09.09.2022

--- js/task-01.js
+++ js/task-01.js
@@ -1,32 +1,10 @@
-const listCategory = document.querySelectorAll('li.item');
+const allItemsEl = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${allItemsEl.length}`);
 
-console.log (`Number of categories: ${listCategory.length}`);
-
-
-const chaildAll = listCategory.forEach(function(name, number){
-return (name, number)
-});
-console.log (chaildAll);
-//const nameCategory = listCategory.querySelectorAll('h2');
-//const numberItems = listCategory.querySelectorAll('li');
-
-
-
-//nameCategory.forEach( => {document.querySelectorAll('h2')});
-    
-
-//const nameCategory = listCategory.lastElementChild;
-
-
-
-//console.log (nameCategory);
-/*console.log(`Category: ${nameCategory[0].textContent}`);
-console.log(`Elements: ${numberItems[0].length}`);
-
-console.log(`Category: ${nameCategory[1].textContent}`);
-
-console.log(`Category: ${nameCategory[2].textContent}`);
-//nameCategory.forEach(element => console.log (element));
-
-//console.log (listCategory);*/
-
+for (let i = 0; i < allItemsEl.length; i += 1) {
+  const categoryTitle = allItemsEl[i].firstElementChild.textContent;
+  const elementsNumber = allItemsEl[i].lastElementChild.children.length;
+  
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsNumber}`);
+  }
--- js/task-02.js
+++ js/task-02.js
@@ -6,5 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-//const itens.forEach (ingredients[] => {document.createElement('li')});
-console.log (itens);
+const productContainer = document.querySelector ('#ingredients');
+for (let i = 0; i < ingredients.length; i += 1) {
+ //console.log(ingredients[i]);
+const itemsLi = document.createElement('li');
+
+//ingredients[i];
+itemsLi.classList.add('item');
+console.log (itemsLi);
+};
+
+/*const elements = ingredients.map(itemsLi);
+
+console.log(elements);
+productContainer.append(...elements);
+console.log (productContainer);*/
