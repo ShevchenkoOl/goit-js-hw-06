42a63619dcd38c5b13d55a590c243bd979b64cab
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-11 23:40:07 +0200

    12.09.2022

---
+++ js/task-04.js
@@ -0,0 +1,30 @@
+const addNumber = document.querySelector('button[data-action="decrement"]');
+const minusNumber = document.querySelector('button[data-action="increment"]');
+
+const counterValue = 0;
+console.log(addNumber);
+console.log(minusNumber);
+
+
+
+
+
+/*targetBtn.addEventListener('click', increment);
+targetBtn.removeEventListener('click', decrement);
+
+function increment(){
+	counterValue +=1;
+  document.getElementById('value').innerHTML = counterValue;};
+  
+function decrement(){
+	counterValue -=1;
+  document.getElementById('value').innerHTML = counterValue;
+  
+}*/
+
+
+
+
+
+
+
