ef6e8f300a3bd1d59130254cf25deee944c31c8c
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-13 17:26:14 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,20 @@
+const textNameInput = document.querySelector('#name-input');
+const textNameOutput = document.querySelector('#name-output');
+
+/*console.log(textNameInput);
+console.log(textNameOutput);*/
+
+
+
+
+
+/*const minusNumber = document.querySelector('button[data-action="decrement"]');
+const addNumber = document.querySelector('button[data-action="increment"]');
+let counterValue = 0;
+let  numberPress = document.querySelector('#value');*/
+
+  textNameOutput.textContent = `${textNameInput}`;
+  
+/*minusNumber.addEventListener('click', () => {
+  numberPress.textContent = `${(counterValue -=1)}`
+});*/
